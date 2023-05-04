966e54175df17b1c4e3dcd8d6633f3bfc9873819
jacob <jacob4evans434@gmail.com>
2023-05-05 00:53:04 +0100

    put ls command into its own module

--- src/cli.rs
+++ src/cli.rs
@@ -5,8 +5,8 @@ use add::add_command;
 mod rm;
 use rm::rm_command;
 
-mod show_usage;
-use show_usage::show_usage;
+mod ls;
+use ls::ls;
 
 use crate::repo::*;
 
@@ -42,35 +42,6 @@ fn get_cli() -> clap::Command {
         )
 
     
-
-}
-
-fn ls(repo : &mut Repository) {
-    println!("Repository - {} \n", repo.root);
-
-    if !repo.managed_programs.is_empty() {
-        for program in &repo.managed_programs {
-            println!("{} | {} configurations ", program.name, program.conifigurations.len());
-            
-            if !program.conifigurations.is_empty() {
-                for config in &program.conifigurations {
-                    println!("{} - {} tracked files", config.name, config.managed_files.len());
-
-                        for f in &config.managed_files {
-                        println!("* {} -> {}", f.file_name, f.hash);
-                    }
-                } 
-            } else {
-                println!("there are no configurations for {}", program.name);
-            }
-            if repo.managed_programs.len() > 1 && repo.managed_programs.last() != Some(program) {
-                println!();
-            }
-        }
-    } else {
-        println!("there are no programs being managed by conman");
-    }
-
 
 }
 
---
+++ src/cli/ls.rs
@@ -0,0 +1,33 @@
+
+use crate::repo::*;
+
+pub fn ls(repo : &mut Repository) {
+    println!("Repository - {} \n", repo.root);
+
+    if !repo.managed_programs.is_empty() {
+        for program in &repo.managed_programs {
+            println!("{} | {} configurations ", program.name, program.conifigurations.len());
+            
+            if !program.conifigurations.is_empty() {
+                for config in &program.conifigurations {
+                    println!("{} - {} tracked files", config.name, config.managed_files.len());
+
+                        for f in &config.managed_files {
+                        println!("* {} -> {}", f.file_name, f.hash);
+                    }
+                } 
+            } else {
+                println!("there are no configurations for {}", program.name);
+            }
+            if repo.managed_programs.len() > 1 && repo.managed_programs.last() != Some(program) {
+                println!();
+            }
+        }
+    } else {
+        println!("there are no programs being managed by conman");
+    }
+
+
+}
+
+
