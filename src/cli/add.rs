811cf9d2e40927e6678fd7dec9105034d7076de4
jacob <jacob4evans434@gmail.com>
2023-05-06 15:27:49 +0100

    reworked add command to still work with a mixture of non-existant files and existant files

--- src/cli/add.rs
+++ src/cli/add.rs
@@ -3,12 +3,13 @@ use crate::repo::*;
 
 pub fn add_command(repo : &mut Repository, program_name : String, config_name : Option<String>, file : Option<Vec<String>>) {
      
-    if file.is_some() {
-        for f in file.clone().unwrap() {
-            if !std::path::Path::new(&f).exists() {
-                println!("provided file does not exist!");
-                std::process::exit(0);
-            }
+    if let Some(v) = file {
+        let existant_files = v.iter().filter(|s| {
+            if std::path::Path::new(s).exists() {return true} else {println!("the file {} does not exist", s); return false}});
+
+        for f in existant_files {
+            println!("adding file: {f}");
+            repo.new_file(program_name.clone(), config_name.clone().unwrap(), f.to_string());
         }
     }
 
@@ -37,13 +38,6 @@ pub fn add_command(repo : &mut Repository, program_name : String, config_name : 
         repo.new_config(&program_name, config_name.clone().unwrap()); 
     }
 
-    if file.is_some() {
-        for f in file.unwrap() {
-            println!("adding file: {f}");
-            repo.new_file(program_name.clone(), config_name.clone().unwrap(), f);
-        }
-    }
-   
 }
 
 
