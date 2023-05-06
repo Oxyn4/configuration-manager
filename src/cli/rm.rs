ffba4067009dd3597977ccefc3871b15d7df3312
jacob <jacob4evans434@gmail.com>
2023-05-06 15:14:57 +0100

    made rm command able to remove multiple files

--- src/cli/rm.rs
+++ src/cli/rm.rs
@@ -1,25 +1,26 @@
 
+use std::path::Path;
+
 use crate::repo::*;
 
 pub fn rm_command(repo :&mut Repository, program_name : String, config_name : Option<String>, file : Option<Vec<String>>) {
-    if file.is_some() {
-        //
-        println!("{:?}", file.clone());
-        println!("{:?}", file.clone().unwrap());
-        for f in file.unwrap() {
-            let string_path_to_repo = repo.root.clone() + "programs/" + &program_name + "/" + &config_name.clone().unwrap() + "/" + &f;
-            println!(" sp {}", string_path_to_repo);
-            let file_path_in_repo = std::path::Path::new(&(string_path_to_repo));
-            if !file_path_in_repo.exists() {
-                println!("provided file: {} does not exist!", f);
-                std::process::exit(0);
-            } else {
-                println!("removing file: {}", f.clone());
-                repo.rm_file(program_name, config_name.unwrap(), f);
-                return;
-            }
+    // make sure the file was given and all 
+    let rr = repo.root.clone();
+    if let Some(v) = file.clone() {
+        let existant_files = v.iter()
+             // filter files that dont exist
+            .filter(|fp| {
+                let string_path_to_repo = format!("{}programs/{}/{}/{}", rr, &program_name, &config_name.clone().unwrap(), &fp);
+                if Path::new(&(string_path_to_repo)).exists() {return true} else {println!("could not find file {}", fp); return false}});
+
+        for f in existant_files {
+            println!("removing file: {}", f.clone());
+            repo.rm_file(program_name.clone(), config_name.clone().unwrap(), f.clone());
         }
+
+        return;
     }
+
 
     let mut program_name_found_in_repository : bool = false;
     for p in &repo.managed_programs.clone() { 
